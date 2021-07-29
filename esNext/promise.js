42ab88d2e2343a53e38692e008a7056253c9852f
Marcos Ribas <contatomarcosribas@gmail.com>
2021-07-28 21:21:05 -0300

    ....

--- esNext/promise.js
+++ esNext/promise.js
@@ -1,12 +1,13 @@
 function falarDepoisDe(segundos, frase){
     return new Promise((resolve, reject) => { //resolve quando a promessa é atendida, e reject quando não
         setTimeout(() => {
-            resolve(frase)
+            resolve(frase, 'abc')
         }, segundos * 1000)
     })
 
 }
-
+console.log('oi')
 falarDepoisDe(3, 'Que legal!')
-    .then(frease => frease.concat('?!?!'))
+    .then((frease) => frease.concat('?!?!'))
     .then(outraFrase => console.log(outraFrase))
+    .catch(e => console.log(e)) // catch é a forma de tratar um erro em promises
