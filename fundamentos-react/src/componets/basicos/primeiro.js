8e7b5463f45aaf354a55fd8775467dcb2a6e8f3b
Marcos Ribas <contatomarcosribas@gmail.com>
2021-08-01 13:39:20 -0300

    ...

---
+++ fundamentos-react/src/componets/basicos/primeiro.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Primeiro(){
+    const msg = "Seja bem vindo!"
+    return( <strong>        
+        <p>Essa função retorna uma String</p>
+        <p>{msg}</p>
+        </strong>
+    )
+}
--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -1,1 +1,30 @@
-    
+import './index.css'
+import ReactDOM from 'react-dom'
+import React from 'react'
+import Primeiro from './componets/basicos/primeiro'
+import { div } from 'prelude-ls'
+
+
+const elemento = document.getElementById('root')
+const tag = <p>Colocando um elemento em uma tag</p>
+
+ReactDOM.render(<h1>Olá, mundo!</h1>, elemento) // dois parâmetros, sendo o primeiro o que quer renderizado, e o segundo o local
+ReactDOM.render(
+    <div>
+        <strong>Primeira página em React</strong>
+    </div>,
+    document.getElementById('texto')
+)
+ReactDOM.render(
+    <div>
+        { tag }
+    </div>,
+    document.getElementById('tag')
+)
+ReactDOM.render( //Chamando uma função
+    <div>
+        <Primeiro></Primeiro>        
+    </div>,
+    document.getElementById('primeiro')
+
+)
