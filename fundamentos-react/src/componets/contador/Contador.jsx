a485257636de44e17f1893e0ef73f8b32e4bc94a
Marcos Ribas <contatomarcosribas@gmail.com>
2021-08-11 09:31:56 -0300

    ...

--- fundamentos-react/src/componets/contador/Contador.jsx
+++ fundamentos-react/src/componets/contador/Contador.jsx
@@ -1,18 +1,40 @@
 import React from "react"
+import "./contador.css"
 
 export default class Contador extends React.Component{
     state = {
-            numero: this.props.numeroInicial,
-        
+            numero: this.props.numeroInicial || 0,
+            passo: this.props.passoInicial || 5,
     }
-    incluir() {
-        this.setState()
+    incluir = () => {
+        this.setState({
+            numero: this.state.numero + this.state.passo
+        })
     }
+    diminuir = () => {
+        this.setState({
+            numero: this.state.numero - this.state.passo
+        })
+    }
+    setPasso = (evento) =>{
+        this.setState({
+            passo: +evento.target.value,
+        })
+ 
+    }
+
+
     render() {
         
         return (
-            <div>
+            <div className="contador">
                 <p>Valor inicial: {this.state.numero}</p>
+                <label htmlFor="passoInput">
+                    <input id="passoInput" type="number" 
+                    value={this.state.passo} onChange={this.setPasso}/>
+                </label>
+                <button onClick={this.incluir}>+</button>
+                <button onClick={this.diminuir}>-</button>
             </div>
         )
     }
