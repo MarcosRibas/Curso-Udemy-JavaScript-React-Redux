3e7ba338b349c723a14b94143d084e2cf3ba3a13
Marcos Ribas <contatomarcosribas@gmail.com>
2021-09-04 14:10:25 -0300

    Finish

--- calculadora/src/main/Calculator.jsx
+++ calculadora/src/main/Calculator.jsx
@@ -7,7 +7,7 @@ const initialState = {
     displayValue: '0',
     clearDisplay: false,
     operation: null,
-    value: [0, 0],
+    values: [0, 0],
     current: 0
 }
 
@@ -26,11 +26,33 @@ export default class Calculator extends Component {
     }
 
     setOperation(operation) {
-        console.log(operation)
+        if (this.state.current === 0) {
+            this.setState({ operation, current: 1, clearDisplay: true })
+        }else {
+            const equals = operation === '='
+            const currentOperation = this.state.operation
+
+            const values = [...this.state.values]
+            try {
+                values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`)
+            } catch(e) {
+                values[0] = this.state.values[0]
+            }            
+            values[1] = 0
+
+            this.setState({
+                displayValue: values[0],
+                operation: equals ? null : operation,
+                current: equals ? 0 : 1,
+                clearDisplay: !equals,
+                values
+            })
+
+        }
     }
 
     addDigit(digito) {
-        if (digito === '.' && this.state.displayValue.includes('.')){
+        if (digito === '.' && this.state.displayValue.includes('.')) {
             return
         }
 
@@ -38,7 +60,7 @@ export default class Calculator extends Component {
             || this.state.clearDisplay
         const currentValue = clearDisplay ? '' : this.state.displayValue
         const displayValue = currentValue + digito
-        this.setState({ displayValue, clearDisplay: false})
+        this.setState({ displayValue, clearDisplay: false })
 
         if(digito !== '.'){
             const i = this.state.current
